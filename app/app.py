e02a54a293492f888c4bc11948a349bf4e9d4545
Julian2 <1922635@stud.hs-mannheim.de>
2023-01-11 23:19:00 +0100

    <3

--- app/app.py
+++ app/app.py
@@ -10,43 +10,50 @@ app = Flask(__name__)
 def index():
 	result = []
 	if request.method == 'POST':
-		id = request.form['id']
-		if id:
-			api = "https://world.openfoodfacts.org/api/2/product/"
+		barcode = request.form['id']
+		better_product_name = ""
+		better_eco_score = 0
+		stores = ""
+		res = requests.get(f'https://world.openfoodfacts.org/api/v0/product/{barcode}')
+		product = res.json()["product"]
+		product_name = product.get("product_name")
+		picture = product.get("image_front_small_url")
+		eco_score = product.get('ecoscore_score')
+		category = product.get('categories_hierarchy')
+		if category and len(category) > 0:
+			category = category[-1]
+			similar_products = get_similar_products(category)
+			if len(similar_products) > 0:
+				products_with_better_ecoscore = []
+				products_with_better_ecoscore_and_stores = []
+				for similar_product in similar_products:
+					if similar_product.get('ecoscore_score') >= eco_score:
+						products_with_better_ecoscore.append(similar_product) if not similar_product.get('stores') \
+							else products_with_better_ecoscore_and_stores.append(similar_product)
+				better_product = max(products_with_better_ecoscore_and_stores
+									 if len(products_with_better_ecoscore_and_stores) > 0
+									 else products_with_better_ecoscore, key=lambda x: x.get('ecoscore_score', 0))
+				better_product_name = better_product.get('product_name')
+				better_eco_score = better_product.get('ecoscore_score')
+				better_product_picture = better_product.get('image_front_small_url')
+				stores = better_product.get('stores')
+				print(better_product)
 
-			#print(id)
-			r = requests.get(f"{api}{id}.json?fields=ecoscore_grade,product_name_de,nutriments,countries")
+		is_best_product = better_product.get('ecoscore_score') == eco_score
+
+		return render_template('index.html', product_name=product_name, picture_src=picture, eco_score=eco_score,
+						   	better_eco_score=better_eco_score, better_product_name=better_product_name,
+							better_product_picture=better_product_picture, stores=stores,
+							is_best_product=is_best_product)
+	return render_template('index.html')
 
 
-			product = r.json()
-			if product.get("status") != 0:
-				product = product.get('product')
-				if product.get('product_name_de'):
-					name = product.get('product_name_de')
-					result.append(f"Produktname: {name}")
-				elif product.get('product_name'):
-					name = product.get('product_name')
-					result.append(f"Produktname: {name}")
-				if product.get('nutriments') and product.get('nutriments').get('energy-kcal_100g'):
-					kcal100 = product.get('nutriments').get('energy-kcal')
-					result.append(f"Kalorien/100g/ml: {kcal100}")
-					kcal = product.get('nutriments').get('energy-kcal_serving')
-					#calories += kcal
-					#result.append(f"Insgesamt hast du {calories} kcal gescannt.") #<--- hier war davor energy :)-hinrik
-				# if product['carbon-footprint_100g']:
-				#	name = product['carbon-footprint_100g']
-				#	print(f"C02: {name}")
-				### Eco score
-				if product.get("ecoscore_grade"):
-					ecoscore_grade = product.get("ecoscore_grade")
-					if ecoscore_grade == "unknown":
-						result.append("Ecoscore: not available")
-					else:
-						result.append(f"Ecoscore: {ecoscore_grade}")
-			else:
-				result.append('Product not available!')
 
-	return render_template('index.html', product=result)
+def get_similar_products(category):
+	res = requests.get(f'https://de.openfoodfacts.org/category/{category}.json?fields=ecoscore_score,product_name,image_front_small_url,stores')
+	products = res.json().get("products")
+	return products
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
